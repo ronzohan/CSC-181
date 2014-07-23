489cbb1a60ef1f54f3ff156b72ed3917d044f3c6
Ron Magno <ron.zohan@gmail.com>
2014-07-24 00:00:48 +0800

    Removed whitespace, added testcase file

--- File.py
+++ File.py
@@ -1,36 +1,29 @@
 import cPickle as pickle
-
 from DataFile import DataFile
 
 class FileOperations:
 	def SaveFileInfo(self,data,filename):
 		# do serialization here
 		pickle.dump(data,open(filename,"wb"))
-		
-
-	def LoadFileInfo(self,filename):
-		## --not yet finished 
-		## --double check pickle arguments
-		##  data.league = pickle(filename,)
-		##
-		return pickle.load(open(filename,"rb"))
-	
+	def LoadFileInfo(self,filename,password):
+		##return the datafile loaded if password matches
+		data =  pickle.load(open(filename,"rb"))
+		if data.password == password:
+			return data
 	def setPassword(self,password):
 		self.password = password
-
 	def getLeagueInfo(self,data):
 		return data.league
 	def getManagerInfo(self,data):
 		return data.manager
 	def getEventInfos(self,data):
 		return data.events	
-	
-my = DataFile("bundesliga","mourinho","win1win2","chelsea")
-my.league =  "bundesliga"
-fileOperation = FileOperations()
-# fileOperation.SaveFileInfo(my,"sample.p")
-my = fileOperation.LoadFileInfo("sample.p")
-print my.league
+# my = DataFile("bundesliga","mourinho","win1win2","chelsea")
+# my.league =  "bundesliga"
+# fileOperation = FileOperations()
+# # fileOperation.SaveFileInfo(my,"sample.p")
+# my = fileOperation.LoadFileInfo("sample.p")
+# print my.league
 
 
 
